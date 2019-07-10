3bf98d8bce3a5943fd36b736a2704730d539e407
Taoz <371956576@qq.com>
2019-07-10 16:40:30 +0800

    feat:优化打包配置，添加清空功能

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  chainWebpack: config => {
-    config.module
-      .rule('vue')
-      .test(/\.vue$/)
-      .use('iview-loader')
-        .loader('iview-loader')
-        .options({
-          prefix: true
-        })
-  }
+    assetsDir: "static",
+    chainWebpack: config => {
+        config.module
+            .rule('vue')
+            .test(/\.vue$/)
+            .use('iview-loader')
+            .loader('iview-loader')
+            .options({
+                prefix: true
+            });
+    }
 }
