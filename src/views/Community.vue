26ed84bac1378a3e52ec43c1e1c411befabbe569
yuangongping <1030617785@qq.com>
2019-07-11 11:36:20 +0800

    fix:修改声明内容，添加社区

---
+++ src/views/Community.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="community">
+        <p>sssssssssssssss</p>
+        <p>sssssssssssssss</p>
+        <p>sssssssssssssss</p>
+        <p>sssssssssssssss</p>
+    </div>
+</template>
+<style lang="scss">
+.community{
+  display: flex;
+  margin: 2% 16% 2% 16%;
+  padding: 20px;
+  box-shadow: 0 10px 30px 0 rgba(0,0,0,.1);
+  
+}
+</style>
+<script>
+export default {
+    
+}
+</script>
+
+
